eb6d72e9be4c161fdb4c11a42b827b6b3248c1de
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-03-31 19:24:00 +0700

    menambahkan struktur dasar program dan input nilai

--- Logika_or.cpp
+++ Logika_or.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 using namespace std;
 
+int main() 
+{
+    float nilM, nilB, rerata;
+    string status;
+
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilM;
+    cout << "Masukkan nilai Bahasa Inggris = ";
+    cin >> nilB;
+
+}
