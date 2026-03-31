aa7d0da0656931e18e44d978e1179f74e115d66a
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-03-31 19:29:44 +0700

    menambahkan logika perhitungan rata-rata dan status kelulusan

--- Logika_and.cpp
+++ Logika_and.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
 using namespace std;
 
+int main() 
+{
+    float nilM, nilB, rerata;
+    string status;
+
+    cout << "Masukkan nilai Matematika = ";
+    cin >> nilM;
+    cout << "Masukkan nilai Bahasa Inggris = ";
+    cin >> nilB;
+
+    rerata = (nilM + nilB) / 2;
+
+    if ((rerata >= 60) && (nilM >= 70))
+        status = "Lulus";
+    else
+        status = "Tidak Lulus";
+
+    cout << "Status kelulusan = " << status << ", dengan nilai rata-rata = " << rerata << endl;
+}
