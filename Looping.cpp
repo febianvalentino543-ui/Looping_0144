3da3e6b0c5593946addec7bb95b802a1fb8b3bd1
febianvalentino543-ui <febianvalentino543@gmail.com>
2026-03-31 19:14:11 +0700

    menambahkan perulangan while

--- Looping.cpp
+++ Looping.cpp
@@ -2,3 +2,30 @@
 #include <ctime>
 using namespace std;
 
+int main() 
+{
+    int x;
+
+    // Perulangan dengan FOR
+    // mencetak tulisan sebanyak 5 kali
+    cout << "PERULANGAN FOR" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << "Teknologi Informasi UMY" << endl;
+    }
+    cout << endl;
+
+    // Untuk mendapatkan urutan angka acak yang berbeda
+    srand(time(0));
+
+    // Perulangan dengan WHILE
+    cout << "PERULANGAN WHILE" << endl;
+    x = 1 + rand() % 10;
+
+    while (x > 5) {
+        cout << "Bilangan acak = " << x << endl;
+        x = rand() % 10;
+    }
+
+    cout << "Bilangan acak while yang terakhir = " << x << endl;
+    cout << endl;
+
